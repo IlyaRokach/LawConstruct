410a6aaad67ff14003258dafa5072456c785319e
lumiandrey <LumiAndrey920>
2019-10-09 13:32:33 +0300

    added minor

--- app/src/main/java/by/europrotocol/activity/LoginActivity.kt
+++ app/src/main/java/by/europrotocol/activity/LoginActivity.kt
@@ -22,7 +22,7 @@ class LoginActivity : AppCompatActivity() {
 
     private fun initClickListeners() {
         btn_sign_in.setOnClickListener {
-            startActivity(Intent(this, ProtocolActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
         }
         btn_sign_up.setOnClickListener {
 //            startActivity(Intent(this, RegistrationActivity::class.java))
--- app/src/main/java/by/europrotocol/activity/SplashActivity.kt
+++ app/src/main/java/by/europrotocol/activity/SplashActivity.kt
@@ -3,6 +3,7 @@ package by.europrotocol.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import by.europrotocol.R
 import by.europrotocol.data.repository.SettingApplication
 
@@ -11,10 +12,13 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        if (SettingApplication.isAuthorization(this)) {
-            startActivity(Intent(this, MainActivity::class.java))
-        } else {
-            startActivity(Intent(this, LoginActivity::class.java))
-        }
+
+        Handler().postDelayed({
+            if (SettingApplication.isAuthorization(this)) {
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+        }, 1000)
     }
 }
